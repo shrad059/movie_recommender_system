cd7c70fe023f77820617eb7a96ad9b3056277483
Shraddha <shraddhasingh059@gmail.com>
2023-02-26 20:21:29 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import streamlit as st
+import pickle
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies=[]
+    for i in movies_list:
+        recommended_movies.append(movies.iloc[i[0]].title)
+    return recommended_movies
+
+
+
+movies_list=pickle.load(open('movie_dict.pkl','rb'))
+movies= pd.DataFrame(movies_list)
+similarity =movies_list=pickle.load(open('similarity.pkl','rb'))
+
+st.title("Movie Recommender System")
+selected_movie_name= st.selectbox(
+    'How would you like to be contacted?',
+    movies['title'].values
+
+)
+if st.button('Recommend'):
+    recommendation =recommend(selected_movie_name)
+    for i in recommendation:
+        st.write(i)
+
